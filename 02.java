323618bc1ff74b2d4e8b9d6dc3a42f7ee5e532a8
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-09-16 22:59:08 -0500

    Add files via upload

---
+++ 02.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String nombre = "Paco";
+        String direccion = "USA";
+        String telefono = "123456789";
+
+        
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
+    }
+}
